6aa900f79d90181c170cd02bf73bfb426f3f1370
damencho <damencho@jitsi.org>
2016-11-29 12:34:54 -0600

    Adds minor versions as variables.

--- get-versions.sh
+++ get-versions.sh
@@ -8,6 +8,10 @@
 #  - jitsi_videobridge, jicofo, jitsi_meet_web, jitsi_meet_web_config, jitsi_meet_prosody
 #    these variables can be used to extract the dependent package versions to the
 #    provided jitsi-meet package version
+#  - jitsi_videobridge_min, jicofo_min, jitsi_meet_web_min,
+#    jitsi_meet_web_config_min, jitsi_meet_prosody_min
+#    these are just the version numbers which normally increments,
+#    without prefix 1.0 and suffix -1
 #  - APT_COMMAND that can be used to install/downgrade to provided jitsi-meet version
 #
 
@@ -30,3 +34,18 @@ getAPTParams () {
 APT_COMMAND="apt-get install "
 getAPTParams "$DEPENDENCIES"
 APT_COMMAND="$APT_COMMAND $RET"
+
+# lests add minor versions that normally increments
+jitsi_videobridge_min=${jitsi_videobridge%-1}
+
+jicofo_min=${jicofo%-1}
+jicofo_min=${jicofo_min#1.0-}
+
+jitsi_meet_web_min=${jitsi_meet_web%-1}
+jitsi_meet_web_min=${jitsi_meet_web_min#1.0.}
+
+jitsi_meet_web_config_min=${jitsi_meet_web_config%-1}
+jitsi_meet_web_config_min=${jitsi_meet_web_config_min#1.0.}
+
+jitsi_meet_prosody_min=${jitsi_meet_prosody%-1}
+jitsi_meet_prosody_min=${jitsi_meet_prosody_min#1.0.}
