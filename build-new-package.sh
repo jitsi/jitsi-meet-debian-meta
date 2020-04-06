0f1f0ca4d9d010c6720d2dbfe722464bcdc3c4ba
damencho <damencho@jitsi.org>
2020-04-06 16:43:07 -0500

    Exports the meta version to be available outside the script.

--- build-new-package.sh
+++ build-new-package.sh
@@ -15,9 +15,10 @@ sed -i.bak  -e "s/__JVB_VER__/$JVB_VER-1/" debian/control
 sed -i.bak  -e "s/__JICOFO_VER__/1.0-$JICOFO_VER-1/" debian/control
 sed -i.bak  -e "s/__JITSI_MEET_WEB_VER__/1.0.$JITSI_MEET_WEB_VER-1/" debian/control
 
+export JITSI_MEET_META_VERSION="2.0.${NEW_JITSI_MEET_VER}"
 export DEBFULLNAME="Jitsi Team"
 export DEBEMAIL="dev@jitsi.org"
-dch -v "2.0.$NEW_JITSI_MEET_VER-1" "Build using jitsi-videobridge: $JVB_VER, jicofo: $JICOFO_VER, jitsi-meet-web: $JITSI_MEET_WEB_VER"
+dch -v "${JITSI_MEET_META_VERSION}-1" "Build using jitsi-videobridge: $JVB_VER, jicofo: $JICOFO_VER, jitsi-meet-web: $JITSI_MEET_WEB_VER"
 dch -D unstable -r ""
 
 dpkg-buildpackage -A -rfakeroot -us -uc
